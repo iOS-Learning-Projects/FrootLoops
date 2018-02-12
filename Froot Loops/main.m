b9c0761fffd048d0c3f2901ec269b6d9f865a82a
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2018-02-12 14:05:27 -0200

    just add

--- Froot Loops/main.m
+++ Froot Loops/main.m
@@ -8,10 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+void playMusic(int, int);
+void newPlayMusic(int);
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        newPlayMusic(5);
     }
     return 0;
+}
+
+void playMusic(int nrRepetitions, int nrElephants) {
+    for (int i = 1; i <= nrRepetitions; i++) {
+        printf("%d elefante(s) incomoda(m) muita gente. ", nrElephants);
+        printf("%d elefantes incomodam muito mais.", ++nrElephants);
+    }
+}
+
+void newPlayMusic(int nrElephants) {
+    printf("1 elefante incomoda muita gente.\n");
+    printf("%d elefantes", nrElephants);
+    for (int i = 1; i <= nrElephants; i++) {
+        printf(" incomodam");
+    }
+    printf(" muito mais.\n");
 }
